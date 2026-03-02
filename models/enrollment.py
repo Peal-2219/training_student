dbbc51d070000a726658237d6d5e8dd03e937262
Peal-2219 <peal.a@wanbuffer.in>
2026-03-02 10:16:10 +0530

    [IMP] test pr request

--- models/enrollment.py
+++ models/enrollment.py
@@ -1,30 +1,43 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class TrainingEnrollment(models.Model):
     _name = 'training.enrollment'
     _description = 'Training Enrollment'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string="Reference", required=True)
+    enrollment_number = fields.Char(
+        string="Enrollment Number",
+        required=True,
+        copy=False,
+        # readonly=True,
+        # default="New"
+    )
 
     student_id = fields.Many2one(
         'training.student',
         string="Student",
-        required=True,
-        ondelete='cascade'
+        required=True
     )
 
     course_id = fields.Many2one(
         'training.course',
         string="Course",
-        required=True,
-        ondelete='cascade'
+        required=True
     )
 
-    enrollment_date = fields.Date(string="Enrollment Date")
+    enrollment_date = fields.Date(default=fields.Date.today)
 
     status = fields.Selection([
         ('draft', 'Draft'),
-        ('ongoing', 'Ongoing'),
-        ('completed', 'Completed')
-    ], default='draft')
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled')
+    ], default='draft', tracking=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('enrollment_number', 'New') == 'New':
+            vals['enrollment_number'] = self.env['ir.sequence'].next_by_code(
+                'training.enrollment'
+            ) or 'New'
+        return super().create(vals)
